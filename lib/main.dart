05b5bdd4cc97208e0d5f88efc0d35282c5e7104b
Timothy Jin <timj26@student.ubc.ca>
2023-04-05 10:52:20 -0700

    Removed while loop login check and replaced with stream subscription check

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_authenticator/amplify_authenticator.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
@@ -58,24 +60,38 @@ class _AppRouterState extends State<AppRouter> {
   void initState() {
     super.initState();
     _checkUserSignIn();
-
   }
-
-
-
 
   Future<void> _checkUserSignIn() async {
     await _configureAmplify();
-
     AuthSession result = await Amplify.Auth.fetchAuthSession();
-
-    while (!result.isSignedIn) {
-      print('CHECKING LOGIN');
-
-      await Future.delayed(const Duration(seconds: 1));
-      result = await Amplify.Auth.fetchAuthSession();
+    if (result.isSignedIn) {
+      initSignedIn();
     }
+    StreamSubscription hubSubscription = Amplify.Hub.listen([HubChannel.Auth], (hubEvent) {
+      print(hubEvent.eventName);
+      switch (hubEvent.eventName) {
+        case "SIGNED_IN":
+          {
+            initNotSignedIn();
+            print("USER IS SIGNED IN");
+          }
+          break;
+        case "SIGNED_OUT":
+          {
+            print("USER IS SIGNED OUT");
+          }
+          break;
+        case "SESSION_EXPIRED":
+          {
+            print("USER IS SIGNED IN");
+          }
+          break;
+      }
+    });
+  }
 
+  Future<void> initNotSignedIn() async {
     // Wait for user group to be assigned if not assigned
 
     AuthSession authSession = await Amplify.Auth.fetchAuthSession(
@@ -130,7 +146,6 @@ class _AppRouterState extends State<AppRouter> {
       }
     }
 
-
     // Route to new page
 
     final Map<String, String> userAttributes = await fetchCurrentUserAttributes();
@@ -160,13 +175,62 @@ class _AppRouterState extends State<AppRouter> {
       } on ApiException catch (e) {
         print('Query failed: $e');
       }
-      if(context.mounted) {
+      if (context.mounted) {
         Navigator.of(context).push(
           PageRouteBuilder(
-            pageBuilder: (_, __, ___) =>
-                PatientApp(
-                  userAttributes: userAttributes,
-                ),
+            pageBuilder: (_, __, ___) => PatientApp(
+              userAttributes: userAttributes,
+            ),
+            transitionDuration: const Duration(milliseconds: 500),
+            transitionsBuilder: (_, a, __, c) => FadeTransition(opacity: a, child: c),
+          ),
+        );
+      }
+    } else if (userGroup == 'care_provider_user' && context.mounted) {
+      Navigator.of(context).push(
+        PageRouteBuilder(
+          pageBuilder: (_, __, ___) => ClinicApp(
+            userAttributes: userAttributes,
+          ),
+          transitionDuration: const Duration(milliseconds: 500),
+          transitionsBuilder: (_, a, __, c) => FadeTransition(opacity: a, child: c),
+        ),
+      );
+    }
+  }
+
+  Future<void> initSignedIn() async {
+    // Wait for user group to be assigned if not assigned
+
+    AuthSession authSession = await Amplify.Auth.fetchAuthSession(
+      options: CognitoSessionOptions(getAWSCredentials: true),
+    );
+    String token = (authSession as CognitoAuthSession).userPoolTokens!.idToken;
+    // Parse the JWT
+    Map<String, dynamic> payload = Jwt.parseJwt(token);
+    // Access the groups
+    String userGroup = payload['cognito:groups'][0];
+
+    while (userGroup != 'patient_user' && userGroup != 'care_provider_user') {
+      print(userGroup);
+      print('CHECKING USER GROUP');
+      await Future.delayed(const Duration(seconds: 1));
+      authSession = await Amplify.Auth.fetchAuthSession(
+        options: CognitoSessionOptions(getAWSCredentials: true),
+      );
+      token = (authSession as CognitoAuthSession).userPoolTokens!.idToken;
+      payload = Jwt.parseJwt(token);
+      userGroup = payload['cognito:groups'][0];
+    }
+
+    final Map<String, String> userAttributes = await fetchCurrentUserAttributes();
+    if (userGroup == 'patient_user') {
+      if (context.mounted) {
+        Navigator.of(context).push(
+          PageRouteBuilder(
+            pageBuilder: (_, __, ___) => PatientApp(
+              userAttributes: userAttributes,
+            ),
             transitionDuration: const Duration(milliseconds: 500),
             transitionsBuilder: (_, a, __, c) => FadeTransition(opacity: a, child: c),
           ),
@@ -203,35 +267,28 @@ class _AppRouterState extends State<AppRouter> {
     }
   }
 
-
   Future<Map<String, String>> fetchCurrentUserAttributes() async {
-    if (authInfo.isEmpty) {
-      print('AUTH INFO EMPTY');
-      final result = await Amplify.Auth.fetchUserAttributes();
-      final data = {for (var e in result) e.userAttributeKey.key: e.value};
-      final authSession = await Amplify.Auth.fetchAuthSession(
-        options: CognitoSessionOptions(getAWSCredentials: true),
-      );
-      String token = (authSession as CognitoAuthSession).userPoolTokens!.idToken;
-      // Parse the JWT
-      Map<String, dynamic> payload = Jwt.parseJwt(token);
-      // Access the groups
-      String userGroup = payload['cognito:groups'][0];
+    print('AUTH INFO EMPTY');
+    final result = await Amplify.Auth.fetchUserAttributes();
+    final data = {for (var e in result) e.userAttributeKey.key: e.value};
+    final authSession = await Amplify.Auth.fetchAuthSession(
+      options: CognitoSessionOptions(getAWSCredentials: true),
+    );
+    String token = (authSession as CognitoAuthSession).userPoolTokens!.idToken;
+    // Parse the JWT
+    Map<String, dynamic> payload = Jwt.parseJwt(token);
+    // Access the groups
+    String userGroup = payload['cognito:groups'][0];
 
-      authInfo = data;
-      authInfo["user_group"] = userGroup;
-      return authInfo;
-    } else {
-      print('AUTH INFO FILLED');
-      return authInfo;
-    }
+    authInfo = data;
+    authInfo["user_group"] = userGroup;
+    return authInfo;
   }
 
   //UI
 
   @override
   Widget build(BuildContext mainCtx) {
-
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
     ]);
@@ -322,8 +379,7 @@ class _AppRouterState extends State<AppRouter> {
           builder: Authenticator.builder(),
           home: const Scaffold(
             body: Center(
-              child:
-              Text('Balance Test',
+              child: Text('Balance Test',
                   style: TextStyle(
                     fontFamily: 'DMSans-Medium',
                     fontWeight: FontWeight.w600,
